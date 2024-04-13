bab6d39f01a0495027f7967f9f5a855e2c65e9e7
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2024-04-13 15:27:13 +0000

    Added the companies UI

--- app/(companies)/page.tsx
+++ app/(companies)/page.tsx
@@ -1,18 +1,55 @@
+//@ts-nocheck
+
 import React from "react";
 import CompanyCard from "../components/CompanyCard";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-function Home() {
+async function fetchCompanies() {
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const res = await fetch(process.env.BASE_URL + "/api/companies", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function Home() {
+  interface Company {
+    id: number;
+    ownerId: string;
+    name: string;
+    description?: string; // The '?' marks this field as optional
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  const companies = await fetchCompanies();
+
   return (
-    <div className="grid grid-cols-4 md:grid-cols-1 gap-4">
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
-      <CompanyCard />
+    <div className="container">
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Societes</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {companies.map((company) => (
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                {company.id}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{company.name}</div>
+              <p className="text-xs text-muted-foreground">{company.name}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- app/components/CompanyCard.jsx
+++ app/components/CompanyCard.jsx
@@ -2,11 +2,11 @@ import { DollarSign } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default function CompanyCard() {
+export default function CompanyCard({ companies }) {
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Test</CardTitle>
+        <CardTitle className="text-sm font-medium">{companies?.name}</CardTitle>
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent>
